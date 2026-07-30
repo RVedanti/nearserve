035bbde0de2ddef71b422428328ade9c9d9e505e
vedanti <vedantirahatikar@gmail.com>
2026-07-30 16:58:22 +0530

    Move vercel.json to project root

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -37,7 +37,9 @@ export const AdminDashboard = () => {
               <span className="font-bold text-red-600 text-sm">A</span>
             </div>
             <div>
-              <p className="font-semibold text-gray-900 text-sm">Admin</p>
+              <p className="font-semibold text-red-600 text-lg">
+  ADMIN TEST
+</p>
               <span className="badge badge-red text-xs">Admin Panel</span>
             </div>
           </div>
